efe7e5d5c476c73eaca4c36057c9b053d9beb0ef
JuanRobles06 <juanrobl3sr3y3s@gmail.com>
2026-05-29 10:59:02 -0600

    Actualizar juego.h

--- BitQuest/juego.h
+++ BitQuest/juego.h
@@ -5,7 +5,7 @@
 #define MAX_VISIBLE_Y 20
 
 struct Jugador {
-	int x, y;
+	int x, y, z;
 	int monedas;
 	int llaves;
 	int cant_pasos;
@@ -14,10 +14,10 @@ struct Jugador {
 void dibujar_informacion(Jugador, int);
 void dibujar_mapa(char*, int, int, Jugador);
 
-void mover_jugador(char*, int, int, int, int);			//Podría ser en asm (movería al player)
-bool verificar_jugador(char*, int, int, int);			//ASM (verificaría la posición del jugador y si es válida)
-bool nivel_completado(char*, int, int);					//Podría ser en asm (verificaría si el jugador llegó a la salida)
-int cantidad_caracter(char*, int, int);					//ASM (devolvería la cantidad de 'caracter' en el mapa)
+void mover_jugador(char*, int, int, int, int);			//PodrÃ­a ser en asm (moverÃ­a al player)
+bool verificar_jugador(char*, int, int, int);			//ASM (verificarÃ­a la posiciÃ³n del jugador y si es vÃ¡lida)
+bool nivel_completado(char*, int, int);					//PodrÃ­a ser en asm (verificarÃ­a si el jugador llegÃ³ a la salida)
+int cantidad_caracter(char*, int, int);					//ASM (devolverÃ­a la cantidad de 'caracter' en el mapa)
 int calcular_puntuaje(int, int, int);					//ASM
 bool verificar_objeto(char*, int, int, int, int);		//ASM
 int celdas_libres(char*, int);							//ASM
